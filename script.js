01195143453b1ff862e728613647badd71fb9400
I-HYEON <tjwn1408@naver.com>
2023-06-05 02:47:26 +0900

    ...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // HTML 문서가 로드된 이후에 실행될 코드를 작성하겠음.
+    // HTML 문서가 로드된 이후에 실행될 코드를 작성.
     var links = document.querySelectorAll('.nav-link');
     // nav-link라는 클래스를 가진 태그를 모두 가져와서 navLinks로 지정
     links.forEach(function(link) {
@@ -8,8 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
         // 이벤트 리스너를 부여할 것.
         const target = event.target
         const href = target.getAttribute('href');
-
-        // 네비게이션바 아이템에 active 클래스 추가
         const navLinks = document.querySelectorAll('.nav-link');
         navLinks.forEach(function(navLink) {
             if (navLink.getAttribute('href') === href) {
